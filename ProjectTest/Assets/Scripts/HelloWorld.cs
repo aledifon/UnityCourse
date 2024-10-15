a36ca3afc992e98f8ac619aa8081646d6879653b
aledifon <aledifon89@gmail.com>
2024-10-15 19:43:01 +0200

    Created ProjectTest for C# Programming tests

---
+++ ProjectTest/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    //Variables
+    int number = 10; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi number is "+ number);
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
